620825e2d0f93d71d0211b38c057fc314398b0be
Vyacheslav_Emelyanenko <evaeyv@mail.ru>
2018-11-01 11:18:05 +0300

    Commit create generate number

---
+++ src/com/company/utils/CardGenerate.java
@@ -0,0 +1,32 @@
+package com.company.utils;
+
+import java.util.Random;
+
+public class CardGenerate {
+    private static final Random RNG = new Random(System.currentTimeMillis());
+
+    private static final int DIGIT_UPPER_LIMIT = 10;
+
+    /**Depends on the type of card*/
+    private int CARD_NUMBER_LENGTH;
+
+    /**
+     * Method generating random number
+     * @param bankIdNumber BIN number card
+     * @param NumberLength length number card
+     * @return number card
+     */
+    public String generate(String bankIdNumber, int NumberLength) {
+        CARD_NUMBER_LENGTH = NumberLength;
+        //int randomNumberLength = CARD_NUMBER_LENGTH - (bankIdNumber.length() + 1);
+        int randomNumberLength = CARD_NUMBER_LENGTH - (bankIdNumber.length());
+
+        StringBuilder builder = new StringBuilder(bankIdNumber);
+
+        for (int i = 0; i < randomNumberLength; i++) {
+            builder.append(RNG.nextInt(DIGIT_UPPER_LIMIT));
+        }
+
+        return builder.toString();
+    }
+}
---
+++ src/com/company/valid/CardValidate.java
@@ -0,0 +1,23 @@
+package com.company.valid;
+
+/**
+ * Abstract class that implements the validate
+ */
+public abstract class CardValidate {
+    protected CardValidate next;
+
+    /**
+     * Method seting next validator
+     * @param validator seting next validator
+     */
+    public void setNext(CardValidate validator) {
+        next = validator;
+    }
+
+    /**
+     * Abstract method validating card
+     * @param cardNumber number card
+     * @return returning true or false
+     */
+    public abstract boolean validateCard(String cardNumber);
+}
