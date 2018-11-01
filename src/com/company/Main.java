a648266bd39b92f66ebb254091123dfa1414573f
Vyacheslav_Emelyanenko <evaeyv@mail.ru>
2018-11-01 14:08:14 +0300

    Commit main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import com.company.utils.CardFactory;
+import com.company.valid.CardValidate;
+import com.company.valid.LuhnValidate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CardValidate validator = new LuhnValidate();
+        for (String typeCard : args) {
+
+            ICard card = CardFactory.getCard(typeCard.toUpperCase());
+
+            String number = card.getNumber();
+
+            System.out.println(card.getClass().getSimpleName() + ": " + number + " " + validator.validateCard(number));
+        }
     }
 }
