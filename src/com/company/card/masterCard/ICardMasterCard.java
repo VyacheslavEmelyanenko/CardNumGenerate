8fda43ceda5c24a91de594ecf40656e5b6a2df1d
Vyacheslav_Emelyanenko <evaeyv@mail.ru>
2018-11-01 17:03:31 +0300

    Commit mastercard I

--- src/com/company/card/masterCard/ICardMasterCard.java
+++ src/com/company/card/masterCard/ICardMasterCard.java
@@ -2,6 +2,9 @@ package com.company.card.masterCard;
 
 import com.company.ICard;
 
+/**
+ * Interface MasterCard
+ */
 public interface ICardMasterCard extends ICard {
 
     String NUMBERFIRST = "5";
